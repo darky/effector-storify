b18c779df7045743b60238591c4bf762e24d14b1
Vladislav Botvin <darkvlados@gmail.com>
2021-11-21 16:06:53 +0300

    $flatAll

--- index.ts
+++ index.ts
@@ -59,3 +59,36 @@ export function ev$all<T, Payload>(
   const $all = createStore<Payload[]>([]).on(ev, (s, ev) => [...s, ev]);
   return Object.assign(ev, { $all });
 }
+
+export function fx$flatAll<T, Params, Done, Fail>(
+  fx: Effect<Params, Done[], Fail> & { $flatAll?: Store<Done[]> } & T
+): Effect<Params, Done[], Fail> & { $flatAll: Store<Done[]> } & T {
+  if (fx.$flatAll) {
+    return fx as Effect<Params, Done[], Fail> & { $flatAll: Store<Done[]> } & T;
+  }
+
+  if (!is.effect(fx)) {
+    throw new Error("Passed parameter is not an Effect");
+  }
+
+  const $flatAll = createStore<Done[]>([]).on(fx.doneData, (s, ev) => [
+    ...s,
+    ...ev,
+  ]);
+  return Object.assign(fx, { $flatAll });
+}
+
+export function ev$flatAll<T, Payload>(
+  ev: Event<Payload[]> & { $flatAll?: Store<Payload[]> } & T
+): Event<Payload[]> & { $flatAll: Store<Payload[]> } & T {
+  if (ev.$flatAll) {
+    return ev as Event<Payload[]> & { $flatAll: Store<Payload[]> } & T;
+  }
+
+  if (!is.event(ev)) {
+    throw new Error("Passed parameter is not an Event");
+  }
+
+  const $flatAll = createStore<Payload[]>([]).on(ev, (s, ev) => [...s, ...ev]);
+  return Object.assign(ev, { $flatAll });
+}
--- test.ts
+++ test.ts
@@ -2,7 +2,14 @@ import { createEffect, createEvent, createStore } from "effector";
 import { test } from "uvu";
 import { equal, throws } from "uvu/assert";
 
-import { ev$all, ev$last, fx$all, fx$last } from "./index.js";
+import {
+  ev$all,
+  ev$flatAll,
+  ev$last,
+  fx$all,
+  fx$flatAll,
+  fx$last,
+} from "./index.js";
 
 test("fx$last - success", () => {
   const fx = fx$last(createEffect<number, number>((x) => x));
@@ -46,6 +53,28 @@ test("ev$all - success", () => {
 
 test("ev$all - error", () => {
   throws(() => ev$all(createStore(0) as any));
+});
+
+test("fx$flatAll - success", () => {
+  const fx = fx$flatAll(createEffect<number, number[]>((x) => [x]));
+  fx(0);
+  fx(1);
+  equal(fx.$flatAll.getState().concat(2), [0, 1, 2]);
+});
+
+test("fx$flatAll - error", () => {
+  throws(() => fx$flatAll(createStore(0) as any));
+});
+
+test("ev$flatAll - success", () => {
+  const ev = ev$flatAll(createEvent<number[]>());
+  ev([0]);
+  ev([1]);
+  equal(ev.$flatAll.getState().concat(2), [0, 1, 2]);
+});
+
+test("ev$flatAll - error", () => {
+  throws(() => ev$flatAll(createStore(0) as any));
 });
 
 test("composition", () => {
